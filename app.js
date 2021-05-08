1cf2c828e28073fd2678ac879f00fbc29f51895a
inabernic <bernic.in@gmail.com>
2021-05-08 12:59:16 +0200

    creation serveur simple

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+console.log('Requette recue');
+next();
+});
+
+app.use((req, res, next) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+module.exports = app;
